4b8f179e13014656163fd9d2c81c5c2f9ab7200b
Rob Nelson <nexisentertainment@gmail.com>
2019-12-12 16:05:18 -0800

    Bunch of fixes.

--- jenkins-deploy.sh
+++ jenkins-deploy.sh
@@ -8,7 +8,14 @@ CACHE_DIR=${ROOT_DIR}/cache
 
 set -x
 rsync -Rrhavvp --chmod=ugo=rw --delete --progress files-upl/ $ROOT_DIR/
+if [ ! -d $POOL_DIR ]; then
+  mkdir -p $POOL_DIR
+fi
+if [ ! -d $CACHE_DIR ]; then
+  mkdir -p $CACHE_DIR
+  chmod -R 766 $CACHE_DIR
+fi
 rm -rf $POOL_DIR/*
 cp -r ${TMP_POOL_DIR}/* ${POOL_DIR}
 rm -rf ${CACHE_DIR}/*
-chmod -R 755 ${POOL_DIR} 
+chmod -R 777 $ROOT_DIR
